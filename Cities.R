7435089b55de5bc951497bb63b19472c506c967a
dayalani <dayalani@prgs.edu>
2020-02-25 19:03:04 -0800

    new map

--- Cities.R
+++ Cities.R
@@ -1,16 +1,39 @@
-## Read in Cities Population in 2018
-cities <- read_excel("./Data/AnnualPopUrbanAggl-20200214054839.xlsx", sheet = "Data", skip = 6)
+##########----------##########----------##########----------##########----------
+
+##########---------- HEADER
+
+##### meta-information
+## Author: Hardika Dayalani (dayalani@rand.org)
+## Creation: 2020-02-25 for Research Poster Presentation 
+## Description: Creates a geocoded dataframe of cities that are either large or
+  ## growing fast for mapping
+
+##### environment set up
+remove( list= objects() )
+
+##### Load Libraries
+library(readxl)
+library(tidygeocoder)
+
+##### Read in data
+## Cities Population in 2018
+cities <- readxl::read_excel("./Data/AnnualPopUrbanAggl-20200214054839.xlsx", sheet = "Data", skip = 6)
+## Cities Growth in 2018
+growth <- readxl::read_excel("./Data/AverageAnnualRateChangeUrbanAggl-20200214055849.xlsx", sheet = "Data", skip = 6)
+
+
+##### Clean Data
 ## Drop extra columns
 cities <- cities[, c(2, 4)]
 ## Clean names
 names(cities) <- c("city_name", "pop")
 
-growth <- read_excel("./Data/AverageAnnualRateChangeUrbanAggl-20200214055849.xlsx", sheet = "Data", skip = 6)
 ## Drop extra columns
 growth <- growth[, c(2, 4)]
 ## Clean names
 names(growth) <- c("city_name", "rate")
 
+##### Reshape Data
 ## Merge City Population & growth rate
 cities <- merge(x = cities, y = growth, by = "city_name")
 
@@ -29,9 +52,15 @@ cities$fast <- cut(cities$rate,
                    breaks = c(0, 2.5, 5, Inf),
                    labels = c("<2.5%", "2.5+%", "5+%"))
 
-## Geocode
+##### Geocode
+
+## Clean name
 cities[cities$city_name == "Ahmadabad","pop"] <- "Ahmedabad"
-cities <- cities %>% geocode(city_name, method = "osm")
+
+## Geocode
+cities <- cities %>% tidygeocoder::geocode(city_name, method = "osm")
+
+## Add missing geocodes manually
 ## Durg-Bhilainagar
 cities[cities$city_name == "Durg-Bhilainagar", "lat"] <- mean(c(21.21, 21.19))
 cities[cities$city_name == "Durg-Bhilainagar", "long"] <- mean(c(81.38,81.28))
@@ -44,23 +73,3 @@ cities[cities$city_name == "Surat", "long"] <- 72.831061
 
 ## save file
 save(cities, file = "Cities.RData")
-
-ggplot()+
-  geom_point(aes(x = 0, y = 0), size = 20, color = cols3[1])+
-  theme_void()
-save_plot(plot = last_plot(), filename = "./dissertation-poster/images/purple.png", base_asp = 1,bg = "transparent")
-
-ggplot()+
-  geom_point(aes(x = 0, y = 0), size = 100, color = cols3[2])+
-  theme_void()
-save_plot(plot = last_plot(), filename = "./dissertation-poster/images/brown.png", base_asp = 1,bg = "transparent")
-
-ggplot()+
-  geom_point(aes(x = 0, y = 0), size = 100, color = cols3[3])+
-  theme_void()
-save_plot(plot = last_plot(), filename = "./dissertation-poster/images/yellow.png", base_asp = 1,bg = "transparent")
-
-ggplot()+
-  geom_point(aes(x = 0, y = 0), size = 100, color = "black")+
-  theme_void()
-save_plot(plot = last_plot(), filename = "./dissertation-poster/images/black.png", base_asp = 1,bg = "transparent")
--- Project Description and Bio.md
+++ Project Description and Bio.md
@@ -1,3 +1,8 @@
+---
+output:
+  word_document: default
+  html_document: default
+---
 ## Project Description
 
 This project seeks to understand how a change in climate patterns will affect seasonal, internal migration patterns in India and how can cities better support their transient work-force. Every year millions of workers in India migrate for a few months within the country looking for work. Increased uncertainty in weather patterns will increase uncertainty in agricultural labor demand and affect this migration pattern. This project will model the relationship between climate uncertainty and changes in migration patterns. It will also create a system map of entitlements available to this population and which entitlements are accessible to workers while they are on the move. This will help us understand the characteristics of this transient population and how can they be empowered to access their entitlements while migrating. 
